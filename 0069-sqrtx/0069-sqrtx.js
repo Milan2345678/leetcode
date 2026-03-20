6ca8b38188b9360c51836897a603eb00c0200e3e
Milan2345678 <milan.22scse1012665@galgotiasuniversity.edu.in>
2026-03-20 12:29:35 +0530

    Time: 2 ms (39.11%), Space: 55.4 MB (86.2%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    if(x === 0 || x === 1)  return x;
+
+    let left = 0;
+    let right = x;
+    let ans = 0;
+
+    while (left <= right){
+        let mid = Math.floor(left + (right - left) / 2);
+
+        if (mid * mid === x){
+            return mid;
+        }
+        else if (mid * mid < x){
+            ans = mid;
+            left = mid + 1;
+        }
+        else{ 
+            right = mid - 1; 
+        }
+    }
+    return ans;
+};
